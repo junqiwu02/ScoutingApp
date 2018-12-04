8c57be61c541b23c1dcdb7c5a994f7374dd51c0a
junqiwu02 <junqiwu02@gmail.com>
2018-12-03 20:38:24 -0500

    added js files and made nav smaller

---
+++ myteam/scripts.js
@@ -0,0 +1,49 @@
+new Chart($('#matchChart'), {
+    type: 'line',
+    data: {
+        labels: ['Quals 6', 'Quals', 'Quals 7', 'Quals 8', 'Quals 9', 'Quals 10', 'Quals 11', 'Quals 12'],
+        datasets: [{
+            data: [4, 1, 10, 1, 10, 11, 13, 14],
+            label: "Switch Placements",
+            borderColor: "#3e95cd",
+            fill: false
+        }, {
+            data: [20, 35, 33, 15, 6, 8, 9, 14],
+            label: "Scale Placements",
+            borderColor: "#8e5ea2",
+            fill: false
+        }
+        ]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Greater Pittsburg Regional'
+        }
+    }
+});
+
+new Chart($('#modalMatchChart'), {
+    type: 'line',
+    data: {
+        labels: ['Quals 6', 'Quals', 'Quals 7', 'Quals 8', 'Quals 9', 'Quals 10', 'Quals 11', 'Quals 12'],
+        datasets: [{
+            data: [4, 1, 10, 1, 10, 11, 13, 14],
+            label: "Switch Placements",
+            borderColor: "#3e95cd",
+            fill: false
+        }, {
+            data: [20, 35, 33, 15, 6, 8, 9, 14],
+            label: "Scale Placements",
+            borderColor: "#8e5ea2",
+            fill: false
+        }
+        ]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Greater Pittsburg Regional'
+        }
+    }
+});
