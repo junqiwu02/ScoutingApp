fccdc540bb432eeb7cc095cc16ea86a69eee0423
junqiwu02 <junqiwu02@gmail.com>
2018-12-06 21:05:26 -0500

    changed theme and changed redirect signin

--- signin/scripts.js
+++ signin/scripts.js
@@ -1,10 +1,24 @@
 function googleSignIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        let token = result.credential.accessToken;
+    firebase.auth().signInWithRedirect(provider);
 
-        console.log(token);
-    });
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
 }
