13d73eac5e480f6a47c7841ed80f3f66eba366d4
junqiwu02 <junqiwu02@gmail.com>
2018-12-08 15:00:46 -0500

    git test

--- signin/scripts.js
+++ signin/scripts.js
@@ -6,19 +6,21 @@ function googleSignIn() {
     firebase.auth().getRedirectResult().then(function(result) {
         if (result.credential) {
           // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
+          let token = result.credential.accessToken;
           // ...
         }
         // The signed-in user info.
-        var user = result.user;
+        let user = result.user;
+
+        console.log(user);
       }).catch(function(error) {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         // The email of the user's account used.
-        var email = error.email;
+        let email = error.email;
         // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+        let credential = error.credential;
         // ...
       });
 }
