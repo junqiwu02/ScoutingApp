10f58fe56862d10ac702d06b69defa3ebf6837fd
junqiwu02 <junqiwu02@gmail.com>
2018-12-08 15:26:23 -0500

    js formatting

--- firebaseinit.js
+++ firebaseinit.js
@@ -1,10 +1,10 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCvVlh9EXsHlyHMoyf5de2HkAGyXD5kyKY",
-    authDomain: "scouting-app-cd27c.firebaseapp.com",
-    databaseURL: "https://scouting-app-cd27c.firebaseio.com",
-    projectId: "scouting-app-cd27c",
-    storageBucket: "scouting-app-cd27c.appspot.com",
-    messagingSenderId: "876475735312"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCvVlh9EXsHlyHMoyf5de2HkAGyXD5kyKY",
+  authDomain: "scouting-app-cd27c.firebaseapp.com",
+  databaseURL: "https://scouting-app-cd27c.firebaseio.com",
+  projectId: "scouting-app-cd27c",
+  storageBucket: "scouting-app-cd27c.appspot.com",
+  messagingSenderId: "876475735312"
+};
+firebase.initializeApp(config);
