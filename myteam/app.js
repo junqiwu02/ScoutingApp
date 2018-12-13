a703c57e071d9323cdb77e932e7208c9e98825f1
junqiwu02 <junqiwu02@gmail.com>
2018-12-12 19:51:35 -0500

    myteam loading funct and nav spacing

--- myteam/app.js
+++ myteam/app.js
@@ -14,52 +14,81 @@ firebase.auth().onAuthStateChanged(function (signedInUser) {
     }
 });
 
-new Chart($('#matchChart'), {
-    type: 'line',
-    data: {
-        labels: ['Quals 6', 'Quals', 'Quals 7', 'Quals 8', 'Quals 9', 'Quals 10', 'Quals 11', 'Quals 12'],
-        datasets: [{
-            data: [4, 1, 10, 1, 10, 11, 13, 14],
-            label: "Switch Placements",
-            borderColor: "#3e95cd",
-            fill: false
-        }, {
-            data: [20, 35, 33, 15, 6, 8, 9, 14],
-            label: "Scale Placements",
-            borderColor: "#8e5ea2",
-            fill: false
-        }
-        ]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Greater Pittsburg Regional'
-        }
-    }
-});
+$(document).ready(function () {
+    let db = firebase.firestore();
+    // Disable deprecated features
+    db.settings({
+        timestampsInSnapshots: true
+    });
 
-new Chart($('#modalMatchChart'), {
-    type: 'line',
-    data: {
-        labels: ['Quals 6', 'Quals', 'Quals 7', 'Quals 8', 'Quals 9', 'Quals 10', 'Quals 11', 'Quals 12'],
-        datasets: [{
-            data: [4, 1, 10, 1, 10, 11, 13, 14],
-            label: "Switch Placements",
-            borderColor: "#3e95cd",
-            fill: false
-        }, {
-            data: [20, 35, 33, 15, 6, 8, 9, 14],
-            label: "Scale Placements",
-            borderColor: "#8e5ea2",
-            fill: false
+
+    db.collection('teams').get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data().autonScoreOnSwitch}`);
+            switch (doc.data().autonScoreOnSwitch) {
+                case -1:
+                    $('#autonScoreOnSwitch')[0].className = 'fas fa-times-circle text-danger';
+                    break;
+                case 0:
+                    $('#autonScoreOnSwitch')[0].className = 'fas fa-minus-circle text-warning';
+                    break;
+                case 1:
+                    $('#autonScoreOnSwitch')[0].className = 'fas fa-check-circle text-success';
+                    break;
+            }
+        });
+        $('.loading').removeClass('loading');
+        $('#loadingScreen').remove();
+    });
+
+
+    new Chart($('#matchChart'), {
+        type: 'line',
+        data: {
+            labels: ['Quals 6', 'Quals', 'Quals 7', 'Quals 8', 'Quals 9', 'Quals 10', 'Quals 11', 'Quals 12'],
+            datasets: [{
+                data: [4, 1, 10, 1, 10, 11, 13, 14],
+                label: "Switch Placements",
+                borderColor: "#3e95cd",
+                fill: false
+            }, {
+                data: [20, 35, 33, 15, 6, 8, 9, 14],
+                label: "Scale Placements",
+                borderColor: "#8e5ea2",
+                fill: false
+            }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Greater Pittsburg Regional'
+            }
         }
-        ]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Greater Pittsburg Regional'
+    });
+
+    new Chart($('#modalMatchChart'), {
+        type: 'line',
+        data: {
+            labels: ['Quals 6', 'Quals', 'Quals 7', 'Quals 8', 'Quals 9', 'Quals 10', 'Quals 11', 'Quals 12'],
+            datasets: [{
+                data: [4, 1, 10, 1, 10, 11, 13, 14],
+                label: "Switch Placements",
+                borderColor: "#3e95cd",
+                fill: false
+            }, {
+                data: [20, 35, 33, 15, 6, 8, 9, 14],
+                label: "Scale Placements",
+                borderColor: "#8e5ea2",
+                fill: false
+            }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Greater Pittsburg Regional'
+            }
         }
-    }
+    });
 });
