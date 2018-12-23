b695c1045a2d5394d4921144d527906a7d686abc
junqiwu02 <junqiwu02@gmail.com>
2018-12-23 00:00:01 -0500

    updateteams endpoint now updates info for all teams from api

--- updateteams/app.js
+++ updateteams/app.js
@@ -5,24 +5,29 @@ db.settings({
 });
 
 const apiKey = 'pRAgaaAkAGpp0ChiPbcy0CIml4JjDefQvb98QtgybJz54ArMEMCLPioOGNx9ZJVB';
-const teamNum = 4930;
-const matchYear = 2018;
+const expectedPages = 20;
 
-function writeTeamInfo(json) {
-    // Add a new document in collection "cities"
-    db.collection("teams").doc('5930').set(json)
-        .then(function () {
-            document.getElementById('text').innerText = 'Success!';
-        })
-        .catch(function (error) {
-            document.getElementById('text').innerText = 'Error: ' + error;
-        });
+// Write multiple teams with one batch
+function writeTeamInfoBatch(json, pageNum) {
+    if (json.length > 0) {
+        // Get a new write batch
+        let batch = db.batch();
+
+        for (let i = 0; i < json.length; i++) {
+            let teamRef = db.collection("teams").doc(`${json[i].team_number}`);
+            batch.set(teamRef, json[i]);
+        }
+
+        // Commit the batch
+        batch.commit()
+            .then(() => console.log(`Batch success for page ${pageNum}`))
+            .catch(err => console.log(`Batch error for page ${pageNum}: ${err}`));
+    }
 }
 
 
-fetch(`https://www.thebluealliance.com/api/v3/team/frc${teamNum}/simple?X-TBA-Auth-Key=${apiKey}`)
-    .then(res => res.json())
-    .then(json => {
-        console.log(json.nickname);
-        writeTeamInfo(json);
-    });
+for (let page = 0; page < expectedPages; page++) {
+    fetch(`https://www.thebluealliance.com/api/v3/teams/${page}?X-TBA-Auth-Key=${apiKey}`)
+        .then(res => res.json())
+        .then(json => writeTeamInfoBatch(json, page));
+}
