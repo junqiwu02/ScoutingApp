15ef92f9f09c15789df1df7517b9c86aa0044cd5
junqiwu02 <junqiwu02@gmail.com>
2018-12-22 23:23:38 -0500

    endpoint changes

--- updateteams/app.js
+++ updateteams/app.js
@@ -4,11 +4,13 @@ db.settings({
     timestampsInSnapshots: true
 });
 
+const apiKey = 'pRAgaaAkAGpp0ChiPbcy0CIml4JjDefQvb98QtgybJz54ArMEMCLPioOGNx9ZJVB';
+const teamNum = 4930;
+const matchYear = 2018;
+
 function writeTeamInfo(json) {
     // Add a new document in collection "cities"
-    db.collection("teams").doc(`${teamNum}`).set({
-        nickname: json.nickname
-    })
+    db.collection("teams").doc('5930').set(json)
         .then(function () {
             document.getElementById('text').innerText = 'Success!';
         })
@@ -17,10 +19,6 @@ function writeTeamInfo(json) {
         });
 }
 
-
-const apiKey = 'pRAgaaAkAGpp0ChiPbcy0CIml4JjDefQvb98QtgybJz54ArMEMCLPioOGNx9ZJVB';
-const teamNum = 4930;
-const matchYear = 2018;
 
 fetch(`https://www.thebluealliance.com/api/v3/team/frc${teamNum}/simple?X-TBA-Auth-Key=${apiKey}`)
     .then(res => res.json())
