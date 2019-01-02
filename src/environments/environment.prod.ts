3cde0be852db873d0d33c19b9dd38f726086ae98
junqiwu02 <junqiwu02@gmail.com>
2019-01-01 19:15:08 -0500

    add firebaseConfig to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+    production: true,
+
+    firebaseConfig: {
+        apiKey: "AIzaSyCvVlh9EXsHlyHMoyf5de2HkAGyXD5kyKY",
+        authDomain: "scouting-app-cd27c.firebaseapp.com",
+        databaseURL: "https://scouting-app-cd27c.firebaseio.com",
+        projectId: "scouting-app-cd27c",
+        storageBucket: "scouting-app-cd27c.appspot.com",
+        messagingSenderId: "876475735312"
+    }
 };
